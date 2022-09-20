f536dd1f87c7a8ab289c972b4022263f666ed42f
lola <102180345+Lolalabi@users.noreply.github.com>
2022-09-20 18:40:10 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const icon = document.querySelector('.icon');
+    const nav = document.querySelector('.topnav');
+    const navLinks = document.querySelectorAll('.topnav li'); 
+   
+    icon.addEventListener('click', () => {
+         //Toggle Nav
+         nav.classList.toggle('nav-active');
+
+         //Animate Links
+         navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = ''
+                 } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+        }
+     
+     });
+      //icon animation
+      icon.classList.toggle('toggle');
+  });
+     
+    
+}
+navSlide();
+
